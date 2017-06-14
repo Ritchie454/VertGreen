089f5dedf01a3b7c9253b31fa90a0eac5c957337
Ritchie454 <jlr454@live.co.uk>
2017-06-14 14:20:10 +0100

    Create BotConstants.java

--- VertGreen/src/main/java/vertgreen/util/BotConstants.java
+++ VertGreen/src/main/java/vertgreen/util/BotConstants.java
@@ -39,7 +39,7 @@ public class BotConstants {
     public static final Color VERTGREEN_COLOR = new Color(2, 224, 9);
     public static final Color VERTYELLOW_COLOR = new Color(255, 246, 5);
     public static final Color VERTRED_COLOR = new Color(255, 5, 5);
-    public static final String BOT_VERSION = "1.8" + GITCOMMIT_ID;
+    public static final String BOT_VERSION = "1.8" + VertGreen.GITCOMMIT_ID;
     public static final String BOT_RELEASE = "RELEASE";
     private BotConstants() {
     }
