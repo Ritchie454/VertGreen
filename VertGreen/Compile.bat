031e004c14fd111c77b5cae14915b34a35ba5079
Ritchie454 <jlr454@live.co.uk>
2017-06-15 20:02:39 +0100

    Removed files from git repository

--- VertGreen/Compile.bat
+++
@@ -1,5 +0,0 @@
-mvn package shade:shade
-xcopy /s "C:\Users\Ritchie__\VertBot\VertBot\config.yaml" "C:\Users\Ritchie__\VertBot\VertBot\target\"
-xcopy /s "C:\Users\Ritchie__\VertBot\VertBot\credentials.yaml" "C:\Users\Ritchie__\VertBot\VertBot\target\"
-xcopy /s "C:\Users\Ritchie__\VertBot\VertBot\Run.bat" "C:\Users\Ritchie__\VertBot\VertBot\target\"
-exit
