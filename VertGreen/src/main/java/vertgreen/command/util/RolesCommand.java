72c598919cd67c20b9b55d4d57c52218b2275963
Ritchie454 <jlr454@live.co.uk>
2017-06-16 14:31:51 +0100

    Role Command BETA

--- VertGreen/src/main/java/vertgreen/command/util/RolesCommand.java
+++ VertGreen/src/main/java/vertgreen/command/util/RolesCommand.java
@@ -14,12 +14,27 @@ import vertgreen.commandmeta.MessagingException;
 import vertgreen.util.BotConstants;
 import java.text.MessageFormat;
 import java.util.*;
-import net.dv8tion.jda.core.Permission;
+import net.dv8tion.jda.core.*;
+import vertgreen.VertGreen;
+import vertgreen.commandmeta.abs.Command;
+import vertgreen.commandmeta.abs.IUtilCommand;
+import vertgreen.feature.I18n;
+import vertgreen.util.ArgumentUtil;
+import net.dv8tion.jda.core.EmbedBuilder;
+import net.dv8tion.jda.core.entities.Guild;
+import net.dv8tion.jda.core.entities.Member;
+import net.dv8tion.jda.core.entities.Message;
+import net.dv8tion.jda.core.entities.TextChannel;
 
+import java.text.MessageFormat;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
 import vertgreen.util.ArgumentUtil;
 import vertgreen.util.TextUtils;
 
-public class PermissionsCommand extends Command implements IUtilCommand {
+public class RolesCommand extends Command implements IUtilCommand {
 
     @Override
     public void onInvoke(Guild guild, TextChannel channel, Member invoker, Message message, String[] args) {
@@ -31,27 +46,27 @@ public class PermissionsCommand extends Command implements IUtilCommand {
         } else {
             target = ArgumentUtil.checkSingleFuzzySearchResult(channel,args[1]);
         }
-        List<Permission> permurl = new ArrayList<>(target.getPermissions()); 
-        Collections.sort(permurl);
-        String sortperms = permurl.toString();
+        String roleurl = target.getRoles().toString(); 
+        //Collections.sort(roleurl);
+        //String sortperms = permurl.toString();
         try {
-             hasteurl = TextUtils.postToHastebin( " - " + sortperms.replace("_", " ").replace("["," ").replace("]", " ").replace(",", "\n - ").toLowerCase(), true) + ".perms";
+             hasteurl = TextUtils.postToHastebin(roleurl, true) + ".roles";
         }
         catch (UnirestException ex) {
-            throw new MessagingException("Couldn't upload permissions to hastebin :(");
+            throw new MessagingException("Couldn't upload roles to hastebin :(");
         }
         //eb.setTitle("Permissions for" + target.getEffectiveName());
         eb.setColor(target.getColor());
         eb.setThumbnail(target.getUser().getAvatarUrl());
-        eb.addField("Permissions for " + target.getEffectiveName(), " " + hasteurl, true);
+        eb.addField("Roles for " + target.getEffectiveName(), " " + hasteurl, true);
         //eb.setFooter("", target.getUser().getAvatarUrl());
         channel.sendMessage(eb.build()).queue();
-        channel.sendMessage("If you can't see embeds, view your permissions here:\n" + hasteurl).queue();
+        channel.sendMessage("If you can't see embeds, view your roles here:\n" + hasteurl).queue();
     }//.
 
     @Override
     public String help(Guild guild) {
         String usage = "{0} {1} @<username>\n#";
-        return usage + "Get permissions for the mentionned user";
+        return usage + "Get the roles for the mentionned user";
     }
 }
