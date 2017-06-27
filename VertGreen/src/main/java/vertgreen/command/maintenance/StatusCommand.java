32ee6470deedc99c629fd8fdec882721af2676a5
Ritchie454 <jlr454@live.co.uk>
2017-06-27 15:19:17 +0100

    Fix for status command
    Temp fix

--- VertGreen/src/main/java/vertgreen/command/maintenance/StatusCommand.java
+++ VertGreen/src/main/java/vertgreen/command/maintenance/StatusCommand.java
@@ -19,7 +19,7 @@ public class StatusCommand extends Command implements IMaintenanceCommand {
         JDA jda = guild.getJDA();
         status = "Status: " + jda.getStatus() + "\n"; 
         ping = "Ping: " + jda.getPing() + "ms\n"; 
-        shard = "Shard: " + jda.getShardInfo().getShardString() + "\n"; 
+     //   shard = "Shard: " + jda.getShardInfo().getShardString() + "\n"; 
         guildn = "Guild: " + guild.getName() +"\n";
         channel.sendMessage("<:online:313956277808005120> All systems green <:online:313956277808005120>\n```" + status + ping + guildn + shard + "```").queue();
     }
